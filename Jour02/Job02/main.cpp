55d79028c441a2cc15129220c730511bf5628137
olivier-portal <olivierportal09@gmail.com>
2025-11-04 15:59:20 +0100

    feat: start job02

---
+++ Jour02/Job02/main.cpp
@@ -0,0 +1,7 @@
+#include "pinguin_move.hpp"
+
+int main() {
+    Pingouin p(2.8, 0.6);
+    p.sePresenter();
+    return 0;
+}
---
+++ Jour02/Job02/pinguin_move.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <string>
+
+class Aquatique {
+    private:
+    double vitesseNage_{0.0}; //Déclaration vitesse en ms
+
+    protected:
+    double getVitesseNage() const;
+    void setVitesseNage(double v);
+
+    virtual void nage() const;
+
+    public:
+    Aquatique() = default;
+    explicit Aquatique(double vNage);
+    virtual ~Aquatique() = default;
+};
+
+class Terrestre {
+    private:
+    double vitesseMarche_{0.0};
+
+    protected:
+    double getVitesseMarche() const;
+    void setVitesseMarche(double v);
+
+    virtual void marche() const;
+
+    public:
+    Terrestre() = default;
+    explicit Terrestre(double vMarche);
+    virtual ~Terrestre() = default;
+};
+
+class Pingouin : public Aquatique, public Terrestre {
+    public:
+    Pingouin(double vNage, double vMarche, double vGlisse = 0.0);
+
+    void sePresenter() const;
+
+    double getVitesseGlisse() const;
+    void setVitesseGlisse(double v);
+
+    private:
+    double vitesseGlisse_{0.0};
+    void nage() const override;
+    void marche() const override;
+};
